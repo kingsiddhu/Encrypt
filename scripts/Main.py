048a4d681386557ec700109cdd0b038b7b14950c
siddharth <72471456+kingsiddhu@users.noreply.github.com>
2022-08-21 10:12:53 -0300

    Add files via upload

--- scripts/Main.py
+++ scripts/Main.py
@@ -1,9 +1,13 @@
+#Encrypton by siddharth
+##V2
+##qwertyuiopasdfghjklzxcvbnm1234567890`-=[];',./ QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()~_+{}|:"<>?
+##`1qaz2wsx3edc4rfv5tgb6yhn7ujm 8ik,9ol.0p;/-['=]~!QAZ@WSX#EDC$RFV%TGB^YHN&UJM*IK<(OL>)P:?_{"+}|
 def Encrypt(phrase, NumOfTimes=1):
     """
     This Encrypts your phrase
     """
     before = r"""qwertyuiopasdfghjklzxcvbnm1234567890`-=]£;',./ QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()~_+{}|:"<>?"""
-    afters = r"""aM6 ABCDE=N[]£127890-defghijklm3VWXYxyz,"{nop4.~!@#$%^Z<>?:5}|_+tbcFGHIJKLuvw&*()OPQRSTUqrs/;'"""
+    afters = r"""ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+`1234567890-=ABCDEFGHIJKLMNOPQRSTUVWXYZ{}|:"<>? ,./;'[]"""
     encrypt = ''
     for en in phrase:
         if en == "\n":
@@ -23,7 +27,7 @@ def Decrypt(phrase):
     This Decrypts your phrase
     """
     before = r"""qwertyuiopasdfghjklzxcvbnm1234567890`-=]£;',./ QWERTYUIOPASDFGHJKLZXCVBNM!@#$%^&*()~_+{}|:"<>?"""
-    afters = r"""aM6 ABCDE=N[]£127890-defghijklm3VWXYxyz,"{nop4.~!@#$%^Z<>?:5}|_+tbcFGHIJKLuvw&*()OPQRSTUqrs/;'"""
+    afters = r"""ABCDEFGHIJKLMNOPQRSTUVWXYZ~!@#$%^&*()_+`1234567890-=ABCDEFGHIJKLMNOPQRSTUVWXYZ{}|:"<>? ,./;'[]"""
     encrypt = ""
     for en in phrase:
         if en == "\n":
@@ -33,6 +37,9 @@ def Decrypt(phrase):
         else:
             encrypt = encrypt + before[afters.find(en)]
     return encrypt
+
+
+
 
 import os
 import time
